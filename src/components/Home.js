fa1bfeb04e82ad08028f13ae2512d4260f04d7a4
Evelyn Resendiz Balderas <chevelynda@hotmail.com>
2024-03-21 01:26:55 -0400

    header image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,9 @@
+import "./Home.css";
+
 const Home = () => {
   return (
-    <section id="homeSection">
-      <h2>Home</h2>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+    <section id="homeSection" className="home-section">
+      <h1 className="home-title">Welcome</h1>
     </section>
   );
 };
